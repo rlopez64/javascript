e4f08f2b9a072ac9c1fcbaead5ce86ac3db96539
Roberto Lopez <rlopez64@ucsc.edu>
2025-03-05 19:27:25 -0800

    added new image files

---
+++ personal_project_1/script.js
@@ -0,0 +1,25 @@
+/* variables */
+let xp = 0;
+let currentClass = 0;
+
+let health = 100;
+let fish = 8;
+
+//weapons
+let currentWeapon = 0;
+let inventory = ["Pebble"];
+
+let day = 1;
+
+/* Connecting the buttons */
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const button4 = document.querySelector("#button4");
+const button5 = document.querySelector("#button5");
+const button6 = document.querySelector("#button6");
+
+/* Connecting the changing text*/
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const classText = document.querySelector("#classText");
