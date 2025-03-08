b2a672f94c66c370542ad932d4474608e8a2369c
Roberto Lopez <rlopez64@ucsc.edu>
2025-03-08 13:23:24 -0800

    updated javascript and html file

--- personal_project_1/script.js
+++ personal_project_1/script.js
@@ -7,9 +7,37 @@ let fish = 8;
 
 //weapons
 let currentWeapon = 0;
-let inventory = ["Pebble"];
+let inventory = [
+  {
+    name: "Pebble",
+    power: 2,
+    cost: 0,
+    description:
+      "A small, smooth stone that fits perfectly in your flipper." +
+      "It’s not much, but with enough determination (or a good throw), it might just leave a mark.",
+  },
+  {
+    name: "Driftwood Club",
+    power: 4,
+    cost: 5,
+    description:
+      "A sturdy piece of driftwood. Not fancy, but it gets the job done.",
+  },
+  {
+    name: "Icicle Dagger",
+    power: 8,
+    cost: 10,
+    description: "A lightweight dagger with a chilling edge. Fast but fragile.",
+  },
+];
 
 let day = 1;
+
+/* Connecting the background image */
+const backgroundImage = document.querySelector("#backgroundImg");
+
+/* connecting every button into one var */
+const buttons = document.querySelectorAll("#controls button");
 
 /* Connecting the buttons */
 const button1 = document.querySelector("#button1");
@@ -19,7 +47,218 @@ const button4 = document.querySelector("#button4");
 const button5 = document.querySelector("#button5");
 const button6 = document.querySelector("#button6");
 
+/* list of all weapons*/
+const weapons_available = [
+  {
+    name: "Driftwood Club",
+    power: 2,
+    cost: 5,
+    description:
+      "A sturdy piece of driftwood. Not fancy, but it gets the job done.",
+  },
+];
+
 /* Connecting the changing text*/
+//personal stats
 const text = document.querySelector("#text");
 const xpText = document.querySelector("#xpText");
 const classText = document.querySelector("#classText");
+const weaponText = document.querySelector("#weaponText");
+const healthText = document.querySelector("#healthText");
+const fishText = document.querySelector("#fishText");
+const dayText = document.querySelector("#dayText");
+
+//monster stats
+const monsterStats = document.querySelector("#monsterStats");
+const monsterName = document.querySelector("#monsterName");
+const monsterHealth = document.querySelector("#monsterHealth");
+
+/* initializing the buttons*/
+button1.onclick = enterTown;
+
+/* total stats */
+const totalStats = {
+  totalFish: 8,
+  totalXP: 0,
+  "total enemy1 killed": 0,
+  "total enemy2 killed": 0,
+  "total enemy3 killed": 0,
+  "total enemy4 killed": 0,
+  "total enemy5 killed": 0,
+};
+
+/* different locations taken based on button */
+const locations = [
+  {
+    name: "go to town square",
+    "num of buttons": 3,
+    "button text": ["Go to Store", "Go to cave", "Check Status"],
+    "button functions": [goStore, goCave, goStats],
+    text:
+      ">You waddle into the bustling heart of town, where penguins of all kinds gather. " +
+      "\n>A faint chill lingers in the air, but the warmth of lively chatter and flickering lanterns makes it feel welcoming",
+    "background image": ["./backgrounds/maplestory/village.jpeg"],
+  },
+  {
+    name: "town square",
+    "num of buttons": 3,
+    "button text": ["Go to Store", "Go to cave", "Check Status"],
+    "button functions": [goStore, goCave, goStats],
+    text: ">It's daytime. You find yourself at the town square, what will you do?",
+    "background image": ["./backgrounds/maplestory/village.jpeg"],
+  },
+  {
+    name: "store",
+    "num of buttons": 3,
+    "button text": ["Buy Item", "Sell Weapon", "Return"],
+    "button functions": [buyItem, sellItem, goTown],
+    text:
+      ">You step into the cozy little shop, the scent of fresh fish and polished gear filling the air. Shelves weapons, and trinkets glisten under the lantern light." +
+      "\n>The shopkeeper, a stout puffin with a monocle, greets you with a nod." +
+      '\n>"Looking to stock up before your big battle? I\'ve got just the thing for a determined penguin like you!"',
+    "background image": ["./backgrounds/maplestory/tavern.jpg"],
+  },
+  {
+    name: "buy menu",
+    "num of buttons": 4,
+    "button text": [
+      "Look at Weapons",
+      "Buy Bread (2 fish)",
+      "Buy Glowing Fish (4 fish)",
+      "Return",
+    ],
+    "button functions": [buyWeapon, buyBread, buyGlowFish, goStore],
+    text:
+      ">You scan the store’s shelves, stocked with useful supplies for a penguin preparing for battle. The shopkeeper flaps over, eager to make a sale." +
+      '\n>"Everything here will help you on your journey. What’ll it be?"',
+    "background image": ["./backgrounds/maplestory/tavern.jpg"],
+  },
+  {
+    name: "weapons menu",
+    "num of buttons": 3,
+    "button text": ["Driftwood Club", "Icicle Dagger", "Return"],
+    "button functions": [buyweapon1, buyweapon2, buyItem],
+    text:
+      ">The shopkeeper waddles over to a sturdy wooden rack, displaying an assortment of finely crafted weapons. " +
+      '\n>"Ah, looking to pack a punch, are we? Choose wisely—your next battle may depend on it!"' +
+      "\n\n[Available Weapons]",
+    "background image": ["./backgrounds/maplestory/smithery.jpeg"],
+  },
+  {
+    name: "stats menu",
+    "num of buttons": 1,
+    "button text": ["Return"],
+    "button functions": [goTown],
+    text: ">Here are your current stats:",
+    "background image": ["./backgrounds/maplestory/retro.jpg"],
+  },
+];
+
+/* transport screen function*/
+function update(location) {
+  //first we change the background
+  backgroundImage.src = location["background image"];
+
+  //first we hide every button
+  for (let i = 0; i < buttons.length; i++) {
+    let button = buttons[i];
+    button.style.display = "none";
+  }
+
+  // then we show only the buttons needed for the current location
+  for (let i = 0; i < location["num of buttons"]; i++) {
+    let current_button = buttons[i];
+    current_button.style.display = "inline-block";
+    current_button.innerText = location["button text"][i];
+    current_button.onclick = location["button functions"][i];
+  }
+
+  text.innerText = location.text;
+}
+
+/* button functions*/
+function goStore() {
+  update(locations[2]);
+}
+
+function goCave() {
+  // ...existing code...
+}
+
+function goInfirmary() {
+  // ...existing code...
+}
+
+function goLodge() {
+  // ...existing code...
+}
+
+function goCombatTrial() {
+  // ...existing code...
+}
+
+function goStats() {
+  update(locations[5]);
+  text.innerText += "\n>Total fish earned: " + totalStats["totalFish"];
+  text.innerText += "\n>Total XP earned: " + totalStats["totalXP"];
+  text.innerText +=
+    "\n>Current Equipment: " +
+    inventory[currentWeapon].name +
+    "(Damage: " +
+    inventory[currentWeapon].power +
+    ")" +
+    "\n(" +
+    inventory[currentWeapon].description +
+    ")";
+  text.innerText +=
+    "\n>Total " + " enemy 1 " + "killed: " + totalStats["total enemy1 killed"];
+  text.innerText +=
+    "\n>Total " + " enemy 2 " + "killed: " + totalStats["total enemy2 killed"];
+  text.innerText +=
+    "\n>Total " + " enemy 3 " + "killed: " + totalStats["total enemy3 killed"];
+}
+
+function enterTown() {
+  update(locations[0]);
+}
+
+function goTown() {
+  update(locations[1]);
+}
+
+function buyItem() {
+  update(locations[3]);
+}
+
+function sellItem() {}
+
+/* function for weapons menu*/
+function buyWeapon() {
+  update(locations[4]);
+  displayWeapons();
+}
+
+/* function to display every weapon available*/
+function displayWeapons() {
+  for (let i = 1; i < inventory.length; i++) {
+    text.innerText +=
+      "\n>" +
+      inventory[i].name +
+      "(Cost: " +
+      inventory[i].cost +
+      " Gold" +
+      ", Damage: " +
+      inventory[i].power +
+      ")" +
+      " - " +
+      inventory[i].description;
+  }
+}
+
+function buyBread() {}
+
+function buyGlowFish() {}
+
+function buyweapon1() {}
+
+function buyweapon2() {}
